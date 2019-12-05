c362e8c06761f2d2adb80f0efaa85f28fdb20f69
Belga-JS <Meherzi.belga@yahoo.com>
2019-12-05 10:18:34 +0100

    Updates

---
+++ precache-manifest.7d7c040024491988f4747453ecea9d61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740cb8abceb821b668f6ac1db20915ba",
+    "url": "/react-card/index.html"
+  },
+  {
+    "revision": "70d5296c3d92d3e80af3",
+    "url": "/react-card/static/css/main.807bd92b.chunk.css"
+  },
+  {
+    "revision": "b10a6777d672e5717166",
+    "url": "/react-card/static/js/2.30c8f78c.chunk.js"
+  },
+  {
+    "revision": "70d5296c3d92d3e80af3",
+    "url": "/react-card/static/js/main.53d0aea1.chunk.js"
+  },
+  {
+    "revision": "050b4a63783a6df22c6b",
+    "url": "/react-card/static/js/runtime-main.10457f20.js"
+  }
+]);
